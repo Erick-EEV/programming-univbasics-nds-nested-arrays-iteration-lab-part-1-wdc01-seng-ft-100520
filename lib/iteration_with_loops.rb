0dfbb6562fbf2570abc99793335e51522f51b620
Erick Villeda <eeverickeev@gmail.com>
2020-09-03 18:22:58 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,8 +1,8 @@
 def find_even_values(src)
 array_of_arrays = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9]
-  ]
-  find_even_values(array_of_arrays)
+  # [1, 2, 3],
+  # [4, 5, 6],
+  # [7, 8, 9]
+  # ]
+  # find_even_values(array_of_arrays)
 end
